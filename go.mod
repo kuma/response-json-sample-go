3f5494bead4eff6d878399278a51abf680049fe4
kuma <k@k.local>
2020-10-19 08:55:06 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kuma/return-json-sample-go
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type user struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+	DefaultPaymentMethod string `json:"default_payment_method"`
+}
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	users := []user{
+		{
+			ID: 10297648,
+			Name: "Kuma Arakawa",
+			Age: 30,
+			DefaultPaymentMethod: "google_pay",
+		},
+		{
+			ID: 10234874,
+			Name: "Nanashi Gonbe",
+			Age: 45,
+			DefaultPaymentMethod: "visa",
+		},
+		{
+			ID: 10297498,
+			Name: "Naoki Smith",
+			Age: 41,
+			DefaultPaymentMethod: "amex",
+		},
+	}
+
+	res, _ := json.Marshal(users)
+
+	fmt.Fprintf(w, string(res))
+}
